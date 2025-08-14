0a03bd9abdc2e94a277bd561aae1dac4acf20ccb
Constantinos Giachalis <cgiachalis@outlook.com>
2025-08-14 22:39:46 +0300

    Use tz = "UTC" in printing tiledb_timestamps

--- R/methods.R
+++ R/methods.R
@@ -43,8 +43,8 @@ print.tiledb_timestamp <- function(x, ...) {
 
 
   ts_char <- vector("character", length = 2)
-  ts_char[1] <- if (length(x$timestamp_start) == 0) "origin" else format(x$timestamp_start)
-  ts_char[2] <- if (length(x$timestamp_end) == 0) format(Sys.time()) else format(x$timestamp_end)
+  ts_char[1] <- if (length(x$timestamp_start) == 0) "origin" else format(x$timestamp_start, tz = "UTC")
+  ts_char[2] <- if (length(x$timestamp_end) == 0) format(Sys.time(), tz = "UTC") else format(x$timestamp_end, tz = "UTC")
 
   txt <- paste0(c("start", "end  "), ": ", cli::col_br_blue(ts_char))
   out <- paste0(" ", cli::col_br_cyan(cli::symbol$bullet), " ", txt, collapse = "\n")
