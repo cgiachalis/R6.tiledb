a04a114f3461e468931271c1744235e6c4504382
Constantinos Giachalis <cgiachalis@outlook.com>
2025-08-18 14:51:41 +0300

    Helper .tiledb_array_close2() that resets timestamps

--- R/tiledb-lib.R
+++ R/tiledb-lib.R
@@ -1,6 +1,17 @@
 
-# .isArray <- function(arr){
-#   methods::is(arr, "tiledb_sparse") || methods::is(arr, "tiledb_dense") || methods::is(arr, "tiledb_array")
-# }
+.isArray <- function(arr){
+  methods::is(arr, "tiledb_sparse") || methods::is(arr, "tiledb_dense") || methods::is(arr, "tiledb_array")
+}
+
+.libtiledb_array_close <- utils::getFromNamespace("libtiledb_array_close", "tiledb")
 
 
+# This reset timestamps slots
+.tiledb_array_close2 <- function(arr) {
+
+  stopifnot(`The 'arr' argument must be a tiledb_array object` = .isArray(arr))
+  arr@timestamp_start <- as.POSIXct(double())
+  arr@timestamp_end <- as.POSIXct(double())
+  .libtiledb_array_close(arr@ptr)
+  arr
+}
