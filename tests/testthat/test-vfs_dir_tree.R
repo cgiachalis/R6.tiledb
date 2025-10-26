22a3d2c615c0e0a0cb18d0ec8ba0d9d92467d50a
Constantinos Giachalis <cgiachalis@outlook.com>
2025-10-26 12:00:02 +0200

    Unit tests for vfs_dir_tree()

---
+++ tests/testthat/test-vfs_dir_tree.R
@@ -0,0 +1,22 @@
+
+test_that("Test 'vfs_dir_tree()'", {
+
+  # TODO: test print/snapshot
+
+  uri_arr <- file.path(withr::local_tempdir(), "test-arr")
+  expect_error(vfs_dir_tree(uri_arr))
+
+  create_empty_test_array(uri_arr)
+  arrobj <- tdb_array(uri_arr)
+  expect_no_error(capture_output(drt <- arrobj$dir_tree()))
+  expect_equal(basename(drt[[1]]), "__commits")
+
+  uri_grp <- file.path(withr::local_tempdir(), "test-grp")
+  create_empty_test_group(uri_grp)
+  grpobj <- tdb_group(uri_grp)
+  expect_no_error(capture_output(drt <- grpobj$dir_tree()))
+  expect_equal(basename(drt[[1]]), "__group")
+  expect_equal(basename(drt[[3]]), "__tiledb_group.tdb")
+  # expect_snapshot(grpobj$dir_tree())
+
+  })
