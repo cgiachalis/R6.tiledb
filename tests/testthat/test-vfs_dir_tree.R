840008e8b78c45029c7d1f0a2137cb80ce88bbdd
Constantinos Giachalis <cgiachalis@outlook.com>
2026-01-30 19:51:08 +0200

    Use expect_contains()

--- tests/testthat/test-vfs_dir_tree.R
+++ tests/testthat/test-vfs_dir_tree.R
@@ -9,14 +9,15 @@ test_that("Test 'vfs_dir_tree()'", {
   create_empty_test_array(uri_arr)
   arrobj <- tdb_array(uri_arr)
   expect_no_error(capture_output(drt <- arrobj$dir_tree()))
-  expect_equal(basename(drt[[1]]), "__commits")
+  expect_contains(basename(drt), c("__commits", "__fragments", "__fragment_meta", "__labels",
+                                     "__meta", "__schema",
+                                     "__enumerations"))
 
   uri_grp <- file.path(withr::local_tempdir(), "test-grp")
   create_empty_test_group(uri_grp)
   grpobj <- tdb_group(uri_grp)
   expect_no_error(capture_output(drt <- grpobj$dir_tree()))
-  expect_equal(basename(drt[[1]]), "__group")
-  expect_equal(basename(drt[[3]]), "__tiledb_group.tdb")
+  expect_contains(basename(drt), c("__group", "__meta", "__tiledb_group.tdb"))
   # expect_snapshot(grpobj$dir_tree())
 
   })
